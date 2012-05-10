9d8e59da260e13d3ea084857a3fc1a05d4f61a59
Oriol Blanc <oriolblanc@gmail.com>
2012-05-10 13:47:32 +0200

    Added new way to create NSInvocation instances

---
+++ OBAdditions/NSInvocation+encapsulate.h
@@ -0,0 +1,21 @@
+//
+//  NSInvocation+encapsulate.h
+//  Gossip
+//
+//  Created by Oriol Blanc Gimeno on 07/05/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSInvocation (encapsulate)
++ (NSInvocation *)invocationForSelector:(SEL)selector target:(id)object;
+
++ (NSInvocation *)invocationWithInvocation:(NSInvocation *)firstInvocation
+                             andInvocation:(NSInvocation *)secondInvocation
+                                parameters:(id)parameters 
+                       customEncapsulation:(NSInvocation *)customInvokeEncapsulation;
+
++ (NSString *)ciao;
+
+@end
---
+++ OBAdditions/NSInvocation+encapsulate.m
@@ -0,0 +1,34 @@
+//
+//  NSInvocation+encapsulate.m
+//  Gossip
+//
+//  Created by Oriol Blanc Gimeno on 07/05/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import "NSInvocation+encapsulate.h"
+
+@implementation NSInvocation (encapsulate)
+
++ (NSInvocation *)invocationForSelector:(SEL)selector target:(id)object
+{
+    NSMethodSignature *signature = [object methodSignatureForSelector:selector];
+	NSInvocation *callbackInvocation = [NSInvocation invocationWithMethodSignature:signature];
+	[callbackInvocation setSelector:selector];
+	[callbackInvocation setTarget:object];
+	[callbackInvocation retainArguments];
+	
+	return callbackInvocation;
+}
+
++ (NSInvocation *)invocationWithInvocation:(NSInvocation *)firstInvocation andInvocation:(NSInvocation *)secondInvocation parameters:(id)parameters customEncapsulation:(NSInvocation *)customInvokeEncapsulation
+{
+    return nil;
+}
+
++ (NSString *)ciao
+{
+    return @"akdsuga";
+}
+
+@end
