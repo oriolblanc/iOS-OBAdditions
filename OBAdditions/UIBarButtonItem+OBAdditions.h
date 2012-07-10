053998188956f7a940dceb36bc6395d1a7443fe6
Oriol Blanc <oriolblanc@gmail.com>
2012-07-10 23:36:16 +0200

    Added UIBarButtonItem additions

---
+++ OBAdditions/UIBarButtonItem+OBAdditions.h
@@ -0,0 +1,13 @@
+//
+//  UIBarButtonItem+OBAdditions.h
+//  Recetas
+//
+//  Created by Oriol Blanc Gimeno on 10/07/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBarButtonItem (OBAdditions)
+
+@end
---
+++ OBAdditions/UIBarButtonItem+OBAdditions.m
@@ -0,0 +1,13 @@
+//
+//  UIBarButtonItem+OBAdditions.m
+//  Recetas
+//
+//  Created by Oriol Blanc Gimeno on 10/07/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import "UIBarButtonItem+OBAdditions.h"
+
+@implementation UIBarButtonItem (OBAdditions)
+
+@end
