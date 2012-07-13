177388f790461dd74ed6d935d9c710e247ba8ab8
Oriol Blanc <oriolblanc@gmail.com>
2012-07-13 11:21:54 +0200

    Perform selector if responds

--- OBAdditions/NSObject+OBAdditions.h
+++ OBAdditions/NSObject+OBAdditions.h
@@ -1,6 +1,5 @@
 //
 //  NSObject+OBAdditions.h
-//  Fever
 //
 //  Created by Oriol Blanc Gimeno on 13/07/12.
 //  Copyright (c) 2012 Fever, Inc. All rights reserved.
@@ -9,5 +8,8 @@
 #import <Foundation/Foundation.h>
 
 @interface NSObject (OBAdditions)
+
+- (id)performSelectorIfResponds:(SEL)aSelector;
+- (id)performSelectorIfResponds:(SEL)aSelector withObject:(id)anObject;
 
 @end
--- OBAdditions/NSObject+OBAdditions.m
+++ OBAdditions/NSObject+OBAdditions.m
@@ -1,6 +1,5 @@
 //
 //  NSObject+OBAdditions.m
-//  Fever
 //
 //  Created by Oriol Blanc Gimeno on 13/07/12.
 //  Copyright (c) 2012 Fever, Inc. All rights reserved.
@@ -9,5 +8,22 @@
 #import "NSObject+OBAdditions.h"
 
 @implementation NSObject (OBAdditions)
+
+- (id)performSelectorIfResponds:(SEL)aSelector
+{
+    if ( [self respondsToSelector:aSelector] ) {
+        return [self performSelector:aSelector];
+    }
+    return NULL;
+}
+
+- (id)performSelectorIfResponds:(SEL)aSelector withObject:(id)anObject
+{
+    if ( [self respondsToSelector:aSelector] ) {
+        return [self performSelector:aSelector withObject:anObject];
+    }
+    return NULL;
+}
+
 
 @end
