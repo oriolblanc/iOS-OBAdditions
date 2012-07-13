51abf43aacf854ac5a4975ab9db001702164d248
Oriol Blanc <oriolblanc@gmail.com>
2012-07-13 11:20:57 +0200

    Added category for NSObject

---
+++ OBAdditions/NSObject+OBAdditions.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+OBAdditions.h
+//  Fever
+//
+//  Created by Oriol Blanc Gimeno on 13/07/12.
+//  Copyright (c) 2012 Fever, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (OBAdditions)
+
+@end
---
+++ OBAdditions/NSObject+OBAdditions.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+OBAdditions.m
+//  Fever
+//
+//  Created by Oriol Blanc Gimeno on 13/07/12.
+//  Copyright (c) 2012 Fever, Inc. All rights reserved.
+//
+
+#import "NSObject+OBAdditions.h"
+
+@implementation NSObject (OBAdditions)
+
+@end
