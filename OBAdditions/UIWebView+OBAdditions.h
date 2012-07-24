cce0234c386091d5eeb090820cf022f776b9b440
Oriol Blanc <oriolblanc@gmail.com>
2012-07-24 12:06:18 +0200

    UIWebView

--- OBAdditions/UIWebView+OBAdditions.h
+++ OBAdditions/UIWebView+OBAdditions.h
@@ -1,13 +1,14 @@
 //
 //  UIWebView+OBAdditions.h
-//  Fever
 //
 //  Created by Oriol Blanc Gimeno on 24/07/12.
-//  Copyright (c) 2012 Fever, Inc. All rights reserved.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
 @interface UIWebView (OBAdditions)
+
+- (void)loadDocument:(NSString*)documentName;
 
 @end
--- OBAdditions/UIWebView+OBAdditions.m
+++ OBAdditions/UIWebView+OBAdditions.m
@@ -1,13 +1,20 @@
 //
-//  UIWebView+OBAdditions.m
-//  Fever
+//  UIWebView+OBAdditions.h
 //
 //  Created by Oriol Blanc Gimeno on 24/07/12.
-//  Copyright (c) 2012 Fever, Inc. All rights reserved.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
 //
 
 #import "UIWebView+OBAdditions.h"
 
 @implementation UIWebView (OBAdditions)
+
+- (void)loadDocument:(NSString*)documentName
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:documentName ofType:nil];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self loadRequest:request];
+}
 
 @end
