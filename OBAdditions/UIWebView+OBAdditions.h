db804170d3932034f233c8b84c1c22dc916a6782
Oriol Blanc <oriolblanc@gmail.com>
2012-07-24 12:06:03 +0200

    added new category for uiwebview

---
+++ OBAdditions/UIWebView+OBAdditions.h
@@ -0,0 +1,13 @@
+//
+//  UIWebView+OBAdditions.h
+//  Fever
+//
+//  Created by Oriol Blanc Gimeno on 24/07/12.
+//  Copyright (c) 2012 Fever, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWebView (OBAdditions)
+
+@end
---
+++ OBAdditions/UIWebView+OBAdditions.m
@@ -0,0 +1,13 @@
+//
+//  UIWebView+OBAdditions.m
+//  Fever
+//
+//  Created by Oriol Blanc Gimeno on 24/07/12.
+//  Copyright (c) 2012 Fever, Inc. All rights reserved.
+//
+
+#import "UIWebView+OBAdditions.h"
+
+@implementation UIWebView (OBAdditions)
+
+@end
