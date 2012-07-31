9fec9a2854481139873a602f9fcdad3455b376a0
Oriol Blanc <oriolblanc@gmail.com>
2012-07-31 12:00:48 +0200

    UIAlertView

---
+++ OBAdditions/UIAlertView+OBAdditions.h
@@ -0,0 +1,13 @@
+//
+//  UIAlertView+OBAdditions.h
+//  Fever
+//
+//  Created by Oriol Blanc on 31/07/12.
+//  Copyright (c) 2012 Fever, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertView (OBAdditions)
+
+@end
---
+++ OBAdditions/UIAlertView+OBAdditions.m
@@ -0,0 +1,13 @@
+//
+//  UIAlertView+OBAdditions.m
+//  Fever
+//
+//  Created by Oriol Blanc on 31/07/12.
+//  Copyright (c) 2012 Fever, Inc. All rights reserved.
+//
+
+#import "UIAlertView+OBAdditions.h"
+
+@implementation UIAlertView (OBAdditions)
+
+@end
