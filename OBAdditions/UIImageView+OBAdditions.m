24d26609e1999867906e82d72268d3507852f067
Oriol Blanc <oriolblanc@gmail.com>
2012-08-03 21:11:38 +0200

    minor

--- OBAdditions/UIImageView+OBAdditions.m
+++ OBAdditions/UIImageView+OBAdditions.m
@@ -1,9 +1,8 @@
 //
 //  UIImageView+OBAdditions.m
-//  Fever
 //
 //  Created by Oriol Blanc Gimeno on 10/05/12.
-//  Copyright (c) 2012 Fever, Inc. All rights reserved.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
 //
 
 #import "UIImageView+OBAdditions.h"
