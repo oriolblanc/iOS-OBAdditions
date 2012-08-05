700d941a44749b82fcfebc4d233b026e3efcd69d
Oriol Blanc <oriolblanc@gmail.com>
2012-08-05 13:19:10 +0200

    Added OBUserDefaults

---
+++ OBAdditions/NSUserDefaults+OBAdditions.h
@@ -0,0 +1,12 @@
+//
+//  NSUserDefaults+OBAdditions.h
+//
+//  Created by Oriol Blanc on 05/08/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSUserDefaults (OBAdditions)
+
+@end
---
+++ OBAdditions/NSUserDefaults+OBAdditions.m
@@ -0,0 +1,12 @@
+//
+//  NSUserDefaults+OBAdditions.m
+//
+//  Created by Oriol Blanc on 05/08/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import "NSUserDefaults+OBAdditions.h"
+
+@implementation NSUserDefaults (OBAdditions)
+
+@end
