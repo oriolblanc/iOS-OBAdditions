5c63913bc4de9bac07b01a6327b19f1e086f41c4
Oriol Blanc <oriolblanc@gmail.com>
2012-09-28 00:22:32 +0200

    Added UIActionSheet category with callback

---
+++ OBAdditions/UIActionSheet+OBAdditions.h
@@ -0,0 +1,12 @@
+//
+//  UIActionSheet+OBAdditions.h
+//
+//  Created by Oriol Blanc on 28/09/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIActionSheet (OBAdditions)
+
+@end
---
+++ OBAdditions/UIActionSheet+OBAdditions.m
@@ -0,0 +1,13 @@
+//
+//  UIActionSheet+OBAdditions.m
+//
+//  Created by Oriol Blanc on 28/09/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import "UIActionSheet+OBAdditions.h"
+
+@implementation UIActionSheet (OBAdditions)
+
+
+@end
