5423761b8590387f8d29aceb49ff8cae00fdebd7
Oriol Blanc <oriolblanc@gmail.com>
2012-10-12 21:25:12 +0200

    Added CLLocationManager additions

---
+++ OBAdditions/CLLocationManager+OBAdditions.h
@@ -0,0 +1,13 @@
+//
+//  CLLocationManager+OBAdditions.h
+//  Gossip
+//
+//  Created by Oriol Blanc on 12/10/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import <CoreLocation/CoreLocation.h>
+
+@interface CLLocationManager (OBAdditions)
+
+@end
---
+++ OBAdditions/CLLocationManager+OBAdditions.m
@@ -0,0 +1,13 @@
+//
+//  CLLocationManager+OBAdditions.m
+//  Gossip
+//
+//  Created by Oriol Blanc on 12/10/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import "CLLocationManager+OBAdditions.h"
+
+@implementation CLLocationManager (OBAdditions)
+
+@end
