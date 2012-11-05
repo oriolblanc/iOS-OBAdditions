ce9a7d25c8cb9424e66f24541f52e84ca9467476
Oriol Blanc <oriolblanc@gmail.com>
2012-11-05 18:14:20 +0100

    Added additions for UITextView

---
+++ OBAdditions/UITextView+OBAdditions.h
@@ -0,0 +1,12 @@
+//
+//  UITextView+OBAdditions.h
+//
+//  Created by Oriol Blanc on 05/11/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITextView (OBAdditions)
+
+@end
---
+++ OBAdditions/UITextView+OBAdditions.m
@@ -0,0 +1,12 @@
+//
+//  UITextView+OBAdditions.m
+//
+//  Created by Oriol Blanc on 05/11/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import "UITextView+OBAdditions.h"
+
+@implementation UITextView (OBAdditions)
+
+@end
