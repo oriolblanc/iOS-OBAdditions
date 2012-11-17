7749fab8511733464890285730013d2e9a5bd8b5
Oriol Blanc <oriolblanc@gmail.com>
2012-11-17 11:58:54 +0100

    Needed imports

--- OBAdditions.h
+++ OBAdditions.h
@@ -26,6 +26,7 @@
 #import "UIImage+OBAdditions.h"
 #import "UIImageView+OBAdditions.h"
 #import "UISearchBar+OBAdditions.h"
+#import "UITextView+OBAdditions.h"
 #import "UIWebView+OBAdditions.h"
 
 // Log methods
