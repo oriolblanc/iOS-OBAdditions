aa61b83137fd0d5ffd1e29d0b63909ce6de8e5fb
Oriol Blanc <oriolblanc@gmail.com>
2012-11-29 18:26:38 +0100

    Added category for UISwitch

---
+++ OBAdditions/UISwitch+OBAdditions.h
@@ -0,0 +1,12 @@
+//
+//  UISwitch+OBAdditions.h
+//
+//  Created by Oriol Blanc on 29/11/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UISwitch (OBAdditions)
+
+@end
---
+++ OBAdditions/UISwitch+OBAdditions.m
@@ -0,0 +1,12 @@
+//
+//  UISwitch+OBAdditions.m
+//
+//  Created by Oriol Blanc on 29/11/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import "UISwitch+OBAdditions.h"
+
+@implementation UISwitch (OBAdditions)
+
+@end
