4519f1096184e1925dd0ce0c5d17ea20381c1798
Oriol Blanc <oriolblanc@gmail.com>
2012-11-29 15:48:04 +0100

    Added NSURL Category

---
+++ OBAdditions/NSURL+OBAdditions.h
@@ -0,0 +1,12 @@
+//
+//  NSURL+OBAdditions.h
+//
+//  Created by Oriol Blanc on 29/11/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSURL (OBAdditions)
+
+@end
---
+++ OBAdditions/NSURL+OBAdditions.m
@@ -0,0 +1,12 @@
+//
+//  NSURL+OBAdditions.m
+//
+//  Created by Oriol Blanc on 29/11/12.
+//  Copyright (c) 2012 Oriol Blanc. All rights reserved.
+//
+
+#import "NSURL+OBAdditions.h"
+
+@implementation NSURL (OBAdditions)
+
+@end
