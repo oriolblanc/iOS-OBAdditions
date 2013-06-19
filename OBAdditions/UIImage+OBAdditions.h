d61f24da309205fe88f08be04762536758142458
Joan Romano <joanromano@gmail.com>
2013-06-19 19:51:40 +0200

    Add grayScaleImage method to UIImage category

--- OBAdditions/UIImage+OBAdditions.h
+++ OBAdditions/UIImage+OBAdditions.h
@@ -7,4 +7,6 @@
 
 @interface UIImage (OBAdditions)
 
+- (UIImage *)grayScaleImage;
+
 @end
--- OBAdditions/UIImage+OBAdditions.m
+++ OBAdditions/UIImage+OBAdditions.m
@@ -64,4 +64,39 @@
 	return image;
 }
 
+- (UIImage *)grayScaleImage
+{
+    // Create image rectangle with current image width/height
+    CGRect imageRect = CGRectMake(0, 0, self.size.width * self.scale, self.size.height * self.scale);
+    // Grayscale color space
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceGray();
+    // Create bitmap content with current image size and grayscale colorspace
+    CGContextRef context = CGBitmapContextCreate(nil, self.size.width * self.scale, self.size.height * self.scale, 8, 0, colorSpace, kCGImageAlphaNone);
+    // Draw image into current context, with specified rectangle
+    // using previously defined context (with grayscale colorspace)
+    CGContextDrawImage(context, imageRect, [self CGImage]);
+    // Create bitmap image info from pixel data in current context
+    CGImageRef grayImage = CGBitmapContextCreateImage(context);
+    // release the colorspace and graphics context
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    // make a new alpha-only graphics context
+    context = CGBitmapContextCreate(nil, self.size.width * self.scale, self.size.height * self.scale, 8, 0, nil, kCGImageAlphaOnly);
+    // draw image into context with no colorspace
+    CGContextDrawImage(context, imageRect, [self CGImage]);
+    // create alpha bitmap mask from current context
+    CGImageRef mask = CGBitmapContextCreateImage(context);
+    // release graphics context
+    CGContextRelease(context);
+    // make UIImage from grayscale image with alpha mask
+    CGImageRef cgImage = CGImageCreateWithMask(grayImage, mask);
+    UIImage *grayScaleImage = [UIImage imageWithCGImage:cgImage scale:self.scale orientation:self.imageOrientation];
+    // release the CG images
+    CGImageRelease(cgImage);
+    CGImageRelease(grayImage);
+    CGImageRelease(mask);
+    // return the new grayscale image
+    return grayScaleImage;
+}
+
 @end
