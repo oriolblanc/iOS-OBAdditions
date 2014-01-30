12f4e5224d86e7bbeb19fdd188cb08d908f52714
Joan Romano <joanromano@gmail.com>
2014-01-30 19:49:45 +0100

    Remove additionalImageNamed:

--- OBAdditions/UIImage+OBAdditions.m
+++ OBAdditions/UIImage+OBAdditions.m
@@ -14,56 +14,6 @@
 
 @implementation UIImage (OBAdditions)
 
-+ (void)load {
-    Class c = (id)self;
-    
-    // Use class_getInstanceMethod for "normal" methods
-    Method m1 = class_getClassMethod(c, @selector(imageNamed:));
-    Method m2 = class_getClassMethod(c, @selector(additionalImageNamed:));
-    
-    // Swap the two methods.
-    method_exchangeImplementations(m1, m2);
-}
-
-+ (UIImage *)additionalImageNamed:(NSString *)imageName
-{
-    NSArray *splitedImageName = [imageName componentsSeparatedByString:@"."];
-    
-    NSString *extension = [splitedImageName objectAtIndex:splitedImageName.count - 1];
-    
-    NSString *fileName = [imageName stringByReplacingOccurrencesOfString:[NSString stringWithFormat:@".%@",extension] withString:@""];
-    
-    if (splitedImageName.count < 2)
-    {
-        ilegalStepWarningWithFormat(@"Ilegal image name");
-        return nil;
-    }
-    
-    UIImage* image = nil;
-    CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
-    if ([UIScreen mainScreen].scale == 2.f && screenHeight == 568.0f)
-    {
-        image = [self additionalImageNamed:[NSString stringWithFormat:@"%@-568h@2x.%@",fileName, extension]];
-    }
-    else
-    {
-        image = [self additionalImageNamed:[NSString stringWithFormat:@"%@-480h@2x.%@",fileName, extension]];
-    }
-
-    
-    if (image == nil)
-    {
-        image = [self additionalImageNamed:imageName];
-    }
-    
-    if (image == nil)
-    {
-        NSLog(@"error al cargar la imagen '%@'", imageName);
-        ilegalStepWarningWithFormat(@"[IMAGE_NOTFOUND] %@", imageName);
-    }
-	return image;
-}
-
 + (UIImage *)imageWithColor:(UIColor *)color
 {
     CGRect rect = CGRectMake(0, 0, 1, 1);
